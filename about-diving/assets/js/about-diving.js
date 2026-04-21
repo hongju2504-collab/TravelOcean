706d1589b2cb7b813a2528f09d0a17bddefbb6bd
hongju2504-collab <hongju2504@gmail.com>
2026-04-21 17:27:02 +0900

    드래그 스와이프
    about-diving
    드래그 스와이프

--- about-diving/assets/js/about-diving.js
+++ about-diving/assets/js/about-diving.js
@@ -42,4 +42,40 @@ document.addEventListener("DOMContentLoaded", () => {
 
     startSlide();
 
+    // 드래그 스와이프
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        slider.classList.add('dragging');
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
+
+    slider.addEventListener('mouseleave', () => {
+        isDown = false;
+        slider.classList.remove('dragging');
+    });
+
+    slider.addEventListener('mouseup', () => {
+        isDown = false;
+        slider.classList.remove('dragging');
+    });
+
+    slider.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 1.5; // 드래그 속도 조절
+        slider.scrollLeft = scrollLeft - walk;
+    });
+
+    slider.addEventListener('mousedown', stopSlide);
+    slider.addEventListener('mouseup', startSlide);
+    slider.addEventListener('mouseleave', startSlide);
+
+    
 });
